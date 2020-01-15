f5b1a08ce682318a3359bf8a88bc873517356502
DeadSpoon <mrdeadspoon@ya.ru>
2020-01-15 10:50:43 +0200

    Add files via upload

--- batch.py
+++ batch.py
@@ -5,10 +5,10 @@ from cassandra.query import BatchStatement
 
 if __name__ == "__main__":
     cluster = Cluster(['127.0.0.1'], port=9042)
-    session = cluster.connect('AutoDocument', wait_for_all_pools=True)
-    session.execute('USE AutoDocument')
+    session = cluster.connect('plain', wait_for_all_pools=True)
+    session.execute('USE plain')
     session.execute(
         "BEGIN BATCH "
-        "UPDATE AutoDocument.Users SET username = 'EUGEN' WHERE id = 1;"
-        "UPDATE AutoDocument.UserDocuments SET username = 'EUGEN' WHERE user_id = 1;"
+        "UPDATE plain.Users SET username = 'artem' WHERE id = 1;"
+        "UPDATE plain.UserDocuments SET username = 'artem' WHERE user_id = 1;"
         "APPLY BATCH;")
